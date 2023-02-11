18f596f326d0924d660dd03ff0868a5588849a55
Bianca Aspin <bianca@mj94.net>
2023-02-11 07:14:13 -0800

    Final clean-up, update classname of navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <ul>
+    <div className="navbar">
       <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink exact to="/movies">Movies</NavLink></li>
       <li><NavLink exact to="/directors">Directors</NavLink></li>
       <li><NavLink exact to="/actors">Actors</NavLink></li>
-    </ul>
+    </div>
   );
 }
 
